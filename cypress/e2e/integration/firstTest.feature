174383a653a32e28bfb08f90dd83f89c61840419
“harbanslal1390” <harbanslal1390@gmail.com>
2024-11-11 16:47:05 +1000

    Started To add Personal Info

--- cypress/e2e/integration/firstTest.feature
+++ cypress/e2e/integration/firstTest.feature
@@ -2,4 +2,6 @@ Feature: Testing Login for standard
  A standard user should Login
  Scenario: A standard use should Login
     Given I am on the login page
+    When I enter the customer first name and last name
+    Then I enter required data in the Personal info page
     
--- cypress/support/step_definitions/firstTestSteps.js
+++ cypress/support/step_definitions/firstTestSteps.js
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
-import { Given } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 const namespace = "4k2zi"; // Replace with your desired namespace (email prefix)
 const apiKey = "fe073a2e-8960-4940-a4b5-42c1df6812f6"; // Replace with your TestMail API key
-const tag = "your-tag"; // Replace with your tag (optional, depending on your TestMail setup)
+const tag = "your-tag6"; // Replace with your tag (optional, depending on your TestMail setup)
 
 // Create a function to retrieve OTP from the TestMail API
 
@@ -15,7 +15,7 @@ Given("I am on the login page", () => {
 
   getOTPFromEmail();
 
-  // Step 3: Use the OTP in your form 
+  // Step 3: Use the OTP in your form
   cy.get("@otp").then((otp) => {
     cy.log(otp);
     console.log(otp);
@@ -23,7 +23,18 @@ Given("I am on the login page", () => {
     cy.get('input[aria-label*="Digit 1"]').type(otp);
   });
 });
+When("I enter the customer first name and last name", () => {
+  cy.wait(10000);
+  cy.get("#mui-1").type("TestDummyFirstName");
+  cy.get("#mui-2").type("TestLastName");
 
+  cy.get('Button[type="submit"]').click();
+});
+
+Then("I enter required data in the Personal info page",()=>{
+  cy.wait(2000)
+  cy.get("button[aria-label='Add Preferred name']").click()
+});
 const getOTPFromEmail = () => {
   const currentDate = Date.now();
   cy.wrap(currentDate).as("currentDate");
@@ -41,9 +52,6 @@ const getOTPFromEmail = () => {
         const otpEmail = res.body.emails[0];
         const otp = otpEmail.subject.substr(0, 4); // Assuming OTP is in the email subject
         cy.wrap(otp).as("otp"); // Save OTP as a Cypress alias for later use
-        cy.get("img[alt='Flight Centre']").should("have.attr", "alt", "Flight Centre")
-        
-        cy.get
       });
     });
   });
